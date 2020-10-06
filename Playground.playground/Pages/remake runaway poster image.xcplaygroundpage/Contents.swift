ae3335e62d7e4cb6da5564f3a5afcc9fe2ada915
mazal <mzhou@lcs.on.ca>
2020-10-06 12:25:02 -0400

    finished runaways poster

--- Playground.playground/Pages/remake runaway poster image.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/remake runaway poster image.xcplaygroundpage/Contents.swift
@@ -62,21 +62,42 @@ canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 
 //Pattern for bottom left
 
-for step in stride(from: 175, through: 13, by: -26 * 2) {
-    
-    step
-    
-    canvas.borderColor = black
-    canvas.drawRectangle(at: Point(x: 0, y: 0), width: step, height: step)
-    
-    if step - 26 > 13 {
-        canvas.borderColor = beige
-        canvas.drawRectangle(at: Point(x: 0, y: 0), width: step - 26, height: step - 26)
+//for step in stride(from: 175, through: 13, by: -26 * 2) {
+//
+//    step
+//
+//    canvas.borderColor = black
+//    canvas.drawRectangle(at: Point(x: 0, y: 0), width: step, height: step)
+//
+//    if step - 26 > 13 {
+//        canvas.borderColor = beige
+//        canvas.drawRectangle(at: Point(x: 0, y: 0), width: step - 26, height: step - 26)
+//
+//    }
+//
+//
+//
+//}
 
-    }
+//Pattern for bottom left
+// Black
+canvas.defaultBorderWidth = 15
+canvas.borderColor = black
+canvas.drawShapesWithFill = false
+for step in stride(from: 350, through: 50, by: -100 ) {
+    
+    canvas.drawRectangle(at: Point(x: 0, y: 0), width: step, height: step, anchoredBy: AnchorPosition.centre)
+    
 
+}
+
+// beige
+canvas.borderColor = beige
+for step in stride(from: 300, through: 50, by: -100 ) {
     
+    canvas.drawRectangle(at: Point(x: 0, y: 0), width: step, height: step, anchoredBy: AnchorPosition.centre)
     
+
 }
 
 //Pattern for bottom right
@@ -121,7 +142,48 @@ for step in stride(from: 300, through: 50, by: -100 ) {
 
 }
 
+//Pattern for top left
+// Black
+canvas.defaultBorderWidth = 15
+canvas.borderColor = black
+canvas.drawShapesWithFill = false
+for step in stride(from: 350, through: 50, by: -100 ) {
+    
+    canvas.drawRectangle(at: Point(x: 0, y: 400), width: step, height: step, anchoredBy: AnchorPosition.centre)
+    
 
+}
+
+// beige
+canvas.borderColor = beige
+for step in stride(from: 300, through: 50, by: -100 ) {
+    
+    canvas.drawRectangle(at: Point(x: 0, y: 400), width: step, height: step, anchoredBy: AnchorPosition.centre)
+    
+
+}
+canvas.drawShapesWithFill = true
+canvas.drawShapesWithBorders = false
+canvas.fillColor = pink
+canvas.drawRectangle(at: Point(x: 200, y: 500), width: 400, height: 200, anchoredBy: AnchorPosition.centre)
+
+
+canvas.textColor = beige
+canvas.drawText(message: "the runaways", at: Point(x: 25, y: 415), size: 50, kerning: 1)
+canvas.textColor = black
+canvas.drawText(message: "friday", at: Point(x: 20, y: 550), size: 10, kerning: 0)
+canvas.drawText(message: "august 19 1977", at: Point(x: 20, y: 535), size: 10, kerning: 0)
+canvas.drawText(message: "tickets $4.50", at: Point(x: 20, y: 520), size: 10, kerning: 0)
+
+
+canvas.drawText(message: "with", at: Point(x: 130, y: 550), size: 10, kerning: 0)
+canvas.drawText(message: "special guests", at: Point(x: 130, y: 535), size: 10, kerning: 0)
+canvas.drawText(message: "wolfgang", at: Point(x: 130, y: 520), size: 10, kerning: 0)
+
+
+canvas.drawText(message: "at ben h. lewis hall", at: Point(x: 280, y: 550), size: 10, kerning: 0)
+canvas.drawText(message: "3443 orange st.", at: Point(x: 280, y: 535), size: 10, kerning: 0)
+canvas.drawText(message: "riverside, california", at: Point(x: 280, y: 520), size: 10, kerning: 0)
 
 //canvas.drawRectangle(at: Point(x: 225, y: 0), width: 175, height: 175)
 //canvas.borderColor = beige
