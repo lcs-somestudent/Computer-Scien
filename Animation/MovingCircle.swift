07545e006c8ab89a927d2e2e197d1c67f73db1f1
Russell Gordon <rgordon@lcs.on.ca>
2020-11-06 09:59:39 -0500

    Added notes on what the parts of a class are.

---
+++ Animation/MovingCircle.swift
@@ -0,0 +1,25 @@
+//
+//  MovingCircle.swift
+//  Animation
+//
+//  Created by Russell Gordon on 2020-11-06.
+//  Copyright © 2020 Royal St. George's College. All rights reserved.
+//
+
+import Foundation
+
+// A class is just a container that stores:
+//  - state (information / data)
+//  - behaviours (methods / functions that make something happen)
+class MovingCircle {
+    
+    // 1. Properties (the state)
+    
+    
+    // 2. Initializer (initialize, or "set up" the properties with a first value)
+    
+    
+    // 3. Methods (make things happen)
+    
+    
+}
